64e79322e6a701ad68e154d54068fe5eff6a999e
Helen Zhao <helen.jiahe.zhao@gmail.com>
2017-05-24 17:00:43 +1200

    added ways to read user input

--- classifyHorse.py
+++ classifyHorse.py
@@ -37,10 +37,32 @@ def classify_horseman(person_speech_list):
 
 def initiate_agent(horsemen_dict):
     #agent talks to user. "please enter your name and your partner's name" "ask for convo log"
-    print("Hi! What is your name?")
+    userName = input("Hi there, what's your name?")
+    partnerName = input("Nice to meet you " + userName + ". What is your partner's name? ")
+    convoFileName = input("Please enter the filename for the conversation log between you and " + partnerName + " you would like me to analyse. ")
+
+    userList = []
+    partnerList = []
+
+    with open(convoFileName + '.txt', 'r') as f:
+        convo = f.readlines()
+        for sentence in convo:
+            name = sentence.split(":")[0]
+            if (name == userName):
+                userList.append(sentence.split(":")[1].strip())
+            else:
+                partnerList.append(sentence.split(":")[1].strip())
 
 
-    #initiate a list for each person based on user input
+
+
+        print(userList)
+        print(partnerList)
+        #print(convo)
+
+
+
+        #initiate a list for each person based on user input
     #read convo log and put each sentence into corresponding person's list (based on first character/word)
     #go through each list and use classify_text() gets us a number for each phrase (number representing the horseman)
     #find the horseman with highest frequency and set that as the result for that person. also calculate the percentage
